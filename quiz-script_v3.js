6e82159580f38ef2fc7a0222773ea3343faaecf9
kzt13 <75968062+kzt13@users.noreply.github.com>
2023-05-02 11:12:10 +0900

    Update quiz-script_v3.js

--- quiz-script_v3.js
+++ quiz-script_v3.js
@@ -62,7 +62,7 @@ function displayNextQuestion() {
   const submitButton = document.getElementById('quiz-submit-button');
   submitButton.addEventListener('click', checkAnswer);
 
-  const answerInput = document.getElementById('answer-input');
+  let answerInput = document.getElementById('answer-input');
   answerInput.addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
       checkAnswer();
