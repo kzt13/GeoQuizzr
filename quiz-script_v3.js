e9c6b4d85c04a0922a0d1bbffa2eef9971dacdb4
kzt13 <75968062+kzt13@users.noreply.github.com>
2023-05-02 10:49:55 +0900

    Update quiz-script_v3.js

--- quiz-script_v3.js
+++ quiz-script_v3.js
@@ -1,5 +1,7 @@
 const directoryPath = './img/';
 let imagePaths = [];
+let questionCount = 0;
+let correctCount = 0;
 
 fetch('br.json')
   .then(response => response.json())
@@ -10,62 +12,56 @@ fetch('br.json')
         answer: item.answer
       };
     });
-
-    const numQuestions = Math.min(imagePaths.length, 10);
-    const questions = [];
-
-    for (let i = 0; i < numQuestions; i++) {
-      const randomIndex = Math.floor(Math.random() * imagePaths.length);
-      const randomImagePath = imagePaths[randomIndex].path;
-      const randomAnswer = imagePaths[randomIndex].answer;
-
-      questions.push({
-        imagePath: randomImagePath,
-        answer: randomAnswer
-      });
-
-      // Remove the image path from the list so it won't be selected again
-      imagePaths.splice(randomIndex, 1);
-    }
-
-    let questionIndex = 0;
-    const questionImage = document.getElementById('quiz-image');
-    const feedbackContainer = document.getElementById('feedback-container');
-    const answerInput = document.getElementById('answer-input');
-    const submitButton = document.getElementById('quiz-submit-button');
-    submitButton.addEventListener('click', checkAnswer);
-
-    function displayQuestion() {
-      questionImage.src = questions[questionIndex].imagePath;
-      feedbackContainer.innerHTML = '';
-      answerInput.value = '';
-    }
-
-    function checkAnswer() {
-      const userAnswer = answerInput.value;
-      const randomAnswer = questions[questionIndex].answer;
-
-      if (userAnswer === randomAnswer.toString()) {
-        feedbackContainer.innerHTML = 'Correct!';
-        feedbackContainer.style.color = 'green';
-      } else {
-        feedbackContainer.innerHTML = 'Incorrect. The correct area code was ' + randomAnswer;
-        feedbackContainer.style.color = 'red';
-      }
-
-      questionIndex++;
-      if (questionIndex < questions.length) {
-        displayQuestion();
-      } else {
-        alert('Quiz complete!');
-      }
-    }
-
-    displayQuestion();
-    answerInput.addEventListener('keyup', function(event) {
-      if (event.key === 'Enter') {
-        checkAnswer();
-      }
-    });
+  })
+  .then(() => {
+    displayNextQuestion();
   })
   .catch(error => console.error(error));
+
+function displayNextQuestion() {
+  // Check if all questions have been answered
+  if (questionCount === Math.min(10, imagePaths.length)) {
+    alert(`You answered ${correctCount} out of ${Math.min(10, imagePaths.length)} questions correctly.`);
+    return;
+  }
+
+  // Select a random image and area code
+  const randomIndex = Math.floor(Math.random() * imagePaths.length);
+  const randomImagePath = imagePaths[randomIndex].path;
+  const randomAnswer = imagePaths[randomIndex].answer;
+
+  // Set question image source
+  const questionImage = document.getElementById('quiz-image');
+  questionImage.src = randomImagePath;
+
+  // Reset feedback container and answer input
+  const feedbackContainer = document.getElementById('feedback-container');
+  feedbackContainer.innerHTML = '';
+  const answerInput = document.getElementById('answer-input');
+  answerInput.value = '';
+
+  // Check user answer and display feedback
+  function checkAnswer() {
+    const userAnswer = answerInput.value;
+    if (userAnswer === randomAnswer.toString()) {
+      feedbackContainer.innerHTML = 'Correct!';
+      feedbackContainer.style.color = 'green';
+      correctCount++;
+    } else {
+      feedbackContainer.innerHTML = 'Incorrect. The correct area code was ' + randomAnswer;
+      feedbackContainer.style.color = 'red';
+    }
+    questionCount++;
+    setTimeout(displayNextQuestion, 1000);
+  }
+
+  const submitButton = document.getElementById('quiz-submit-button');
+  submitButton.addEventListener('click', checkAnswer);
+
+  const answerInput = document.getElementById('answer-input');
+  answerInput.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+      checkAnswer();
+    }
+  });
+}
