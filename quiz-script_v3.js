6bbef9469fd0d4390cabab20dbfa4bd6ffdc65ec
kzt13 <75968062+kzt13@users.noreply.github.com>
2023-05-16 21:27:01 +0900

    Update quiz-script_v3.js

--- quiz-script_v3.js
+++ quiz-script_v3.js
@@ -121,6 +121,7 @@ function displayNextQuestion() {
   // Set question text
   const questionText = document.getElementById('quiz-question');
   if (questionText) {
+    console.log(typeof randomAnswer);
     if (areaCodesData && areaCodesData[randomAnswer]) {
       // 地名データが存在し、かつ該当の地名がある場合、地名を取得して問題文を作成
       const areaName = Object.values(areaCodesData[randomAnswer])[0];
